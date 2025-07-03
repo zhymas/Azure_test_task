721aa96402dc70190bed3b6647723bccd7aa8588
zhymas <shvachko319@gmail.com>
2025-07-03 13:14:51 +0300

    Update function

--- BlobEvent/__init__.py
+++ BlobEvent/__init__.py
@@ -1,6 +1,8 @@
 import logging
 import json
 import azure.functions as func
+from azure.storage.blob import BlobServiceClient
+import os
 
 def main(event: func.EventGridEvent):
     try:
@@ -10,6 +12,12 @@ def main(event: func.EventGridEvent):
         version_id = event_data.get('versionId', None)
         blob_name = blob_url.split('/')[-1]
         container_name = blob_url.split('/')[3] if len(blob_url.split('/')) > 3 else ''
+
+        if version_id is None:
+            blob_service_client = BlobServiceClient.from_connection_string(conn_str=os.getenv('AZURE_STORAGE_CONNECTION_STRING'))
+            container_client = blob_service_client.get_container_client(container=container_name)
+            blob_client = container_client.get_blob_client(blob=blob_name)
+            version_id = blob_client.get_blob_properties().version_id
 
         logging.info(f"Event data: {json.dumps(event_data, indent=2)}")
         logging.info(f"Blob URL: {blob_url}")
